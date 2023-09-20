9782b349cb33e7d5670cfba9d7a0f920332eacc8
Nassim Zen <hironichu@pm.me>
2023-09-21 01:16:45 +0200

    Trying to fix custom lib loading in CI

--- utils/download_lib.ts
+++ utils/download_lib.ts
@@ -2,21 +2,23 @@ import "https://deno.land/std@0.201.0/dotenv/load.ts";
 const LIB_NAME = "webtransport";
 let LIB_URL: URL | undefined;
 
-if (!Deno.env.get("DEVELOPMENT")) {
-    if (!Deno.env.get("DENO_AUTH_TOKENS")) {
-        throw new Error("Please set DENO_AUTH_TOKENS to your auth tokens");
+if (!Deno.env.has("DEVELOPMENT")) {
+    const headers = new Headers();
+    if (Deno.env.has("DENO_AUTH_TOKENS")) {
+        headers.set(
+            "Authorization",
+            `token ${
+                Deno.env.get("DENO_AUTH_TOKENS")!.split("@")[0].split(
+                    ":",
+                )[1]
+            }`,
+        );
     }
+    headers.set("Accept", "application/vnd.github.v3+json");
     const data = await fetch(
         `https://api.github.com/repos/hironichu/${LIB_NAME}/releases`,
         {
-            headers: {
-                Authorization: `token ${
-                    Deno.env.get("DENO_AUTH_TOKENS")!.split("@")[0].split(
-                        ":",
-                    )[1]
-                }`,
-                Accept: "application/vnd.github.v3+json",
-            },
+            headers: headers,
         },
     );
 
@@ -36,7 +38,9 @@ if (!Deno.env.get("DEVELOPMENT")) {
                 `${LIB_NAME}_${Deno.build.os}_${Deno.build.arch}`
             ][0].url,
     );
-    LIB_URL!.username = Deno.env.get("DENO_AUTH_TOKENS")!.split("@")[0]!;
+    LIB_URL!.username = Deno.env.has("DENO_AUTH_TOKENS")
+        ? Deno.env.get("DENO_AUTH_TOKENS")!.split("@")[0]!
+        : "";
 }
 const build_target = Deno.env.get("DEVELOPMENT") ? "debug" : "release";
 LIB_URL = LIB_URL ?? new URL(`../target/${build_target}/`, import.meta.url);
