1f6c86a04874a43498e7fc46720f080290cc71f9
Your Name <you@example.com>
2025-03-13 20:53:45 +0100

    bump version and lint

--- mod.ts
+++ mod.ts
@@ -2,8 +2,8 @@ export * from "./src/webtransport.ts";
 export { ServerConfig } from "./src/config.ts";
 
 export {
-    type BidirectionalStream,
-    type ReceiveStream,
-    type SendStream,
-    StreamID,
+  type BidirectionalStream,
+  type ReceiveStream,
+  type SendStream,
+  StreamID,
 } from "./src/types/streamdef.ts";
